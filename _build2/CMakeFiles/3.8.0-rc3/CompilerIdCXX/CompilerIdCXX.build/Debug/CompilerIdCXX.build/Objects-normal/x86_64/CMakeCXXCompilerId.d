49a83467f4141b707a2ea2230e9126787929c685
Seth Balodi <shashwb@vt.edu>
2017-04-20 11:19:23 -0400

    changes- Parser 55%

---
+++ _build2/CMakeFiles/3.8.0-rc3/CompilerIdCXX/CompilerIdCXX.build/Debug/CompilerIdCXX.build/Objects-normal/x86_64/CMakeCXXCompilerId.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/sethbalodi/CODE/project3-shashwb/_build2/CMakeFiles/3.8.0-rc3/CompilerIdCXX/CMakeCXXCompilerId.cpp
--- unittests.cpp
+++ unittests.cpp
@@ -6,30 +6,30 @@
 // These are just a few examples from my solution and **should be removed**.
 // Depending on your code design your class and functions names would differ
 
-#include "geometry.hpp"
+// #include "geometry.hpp"
 
-TEST_CASE( "Test Basic Geometry: Vec3d ", "[geometry]" ) {
+// TEST_CASE( "Test Basic Geometry: Vec3d ", "[geometry]" ) {
 
-  Vec3d a(2,0,0), b(0,4,0);
+//   Vec3d a(2,0,0), b(0,4,0);
   
-  REQUIRE(a.x == Approx(2));
-  REQUIRE(a.y == Approx(0));
-  REQUIRE(a.z == Approx(0));
+//   REQUIRE(a.x == Approx(2));
+//   REQUIRE(a.y == Approx(0));
+//   REQUIRE(a.z == Approx(0));
 
-  REQUIRE(b.x == Approx(0));
-  REQUIRE(b.y == Approx(4));
-  REQUIRE(b.z == Approx(0));
+//   REQUIRE(b.x == Approx(0));
+//   REQUIRE(b.y == Approx(4));
+//   REQUIRE(b.z == Approx(0));
 
-  double adotb = dot(a,b);
-  REQUIRE(adotb == Approx(0));
+//   double adotb = dot(a,b);
+//   REQUIRE(adotb == Approx(0));
 
-  Vec3d anorm = norm(a);
-  REQUIRE(anorm.x == Approx(1));
-  REQUIRE(anorm.y == Approx(0));
-  REQUIRE(anorm.z == Approx(0));
+//   Vec3d anorm = norm(a);
+//   REQUIRE(anorm.x == Approx(1));
+//   REQUIRE(anorm.y == Approx(0));
+//   REQUIRE(anorm.z == Approx(0));
 
-  Vec3d bnorm = norm(b);
-  REQUIRE(bnorm.x == Approx(0));
-  REQUIRE(bnorm.y == Approx(1));
-  REQUIRE(bnorm.z == Approx(0));
-}
+//   Vec3d bnorm = norm(b);
+//   REQUIRE(bnorm.x == Approx(0));
+//   REQUIRE(bnorm.y == Approx(1));
+//   REQUIRE(bnorm.z == Approx(0));
+// }
